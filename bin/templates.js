797bc8e7678fb7c2418b63a05008040c8558ca8a
周登辉 <15690329227@163.com>
2023-07-20 14:22:09 +0800

    第一次提交

---
+++ bin/templates.js
@@ -0,0 +1,16 @@
+/** 暴露模版代码 */
+module.exports = [
+    {
+      name: 'code-admin',
+      value: 'https://github.com/mistyrain-jiangnan/code-admin'
+    },
+    {
+      name: '30-Days-Of-React',
+      value: 'https://github.com/mistyrain-jiangnan/30-Days-Of-React'
+    },
+    {
+      name: 'dumi2-demo',
+      value: 'https://github.com:guojiongwei/dumi2-demo'
+    }
+  ]
+  
